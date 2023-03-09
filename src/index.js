123c07d0503861b5b915ea49b3165115c0c31d00
yen <l.yennie.j@gmail.com>
2023-03-09 21:22:57 +0900

    rename

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import App from "./App";
+import App from "./app";
 import NotFound from "./pages/NotFound";
 import VideoList from "./pages/VideoList/VidoeList";
 import VideoDetail from "./pages/VideoDetail/VideoDetail";
