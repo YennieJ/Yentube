45d3a7dd2afe55ea6edb430105ae357f4025c70b
yen <l.yennie.j@gmail.com>
2023-03-09 19:34:38 +0900

    prepare deploy

--- src/context/YoutubeApiContext.jsx
+++ src/context/YoutubeApiContext.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { createContext } from "react";
 import Youtube from "../api/youtube";
 import FakeYoutube from "../api/fakeYoutubeClient";
-import YoutubeClinet from "../api/youtubeClient";
+// import YoutubeClinet from "../api/youtubeClient";
 
 export const YoutubeApiContext = createContext();
 
-const client = new YoutubeClinet();
+// const client = new YoutubeClinet();
 const fake = new FakeYoutube();
 
 const youtube = new Youtube(fake);
